c1f55cb211b21ed7f831b457a882dc7d0917d2cb
kosta709 <kosta777@gmail.com>
2020-11-11 12:01:55 +0200

    version with jobs

--- files/installer.sh
+++ files/installer.sh
@@ -2,10 +2,6 @@
 #
 
 echo "$(date) - Entering $0"
-if [[ -z "${CODEFRESH_TOKEN}" ]]; then
-  echo "ERROR: CODEFRESH_TOKEN env is empty"
-  exit 1
-fi
 
 VALUES_FILE=/etc/codefresh/runner-values.yaml
 if [[ ! -f "${VALUES_FILE}" ]]; then
@@ -18,10 +14,30 @@ ACTION=${1:-init}
 
 if [[ "${ACTION}" == "init" ]]; then
   ACTION_PARAMETERS="--exec-demo-pipeline false"
+
+elif [[ "${ACTION}" == "delete" ]]; then
+  if [[ -z "$CODEFRESH_TOKEN" ]]; then
+    CODEFRESH_TOKEN=$( cat ${VALUES_FILE} | yq -r '.Token // empty')
+  fi
+
+  CODEFRESH_HOST=$( cat ${VALUES_FILE} | yq -r '.CodefreshHost // empty')
+  if [[ -z "${CODEFRESH_HOST}" ]]; then
+    CODEFRESH_HOST=https://g.codefresh.io
+  fi
+  codefresh auth create-context --url ${CODEFRESH_HOST} --api-key ${CODEFRESH_TOKEN}
+  
+fi
+
+if [[ -n "${CODEFRESH_TOKEN}" ]]; then
+   CODEFRESH_TOKEN_PARAM="--set Token=${CODEFRESH_TOKEN}"
+   echo "CODEFRESH_TOKEN env is set"
 fi
 
 # COMMAND="codefresh runner init -y --values ${VALUES_FILE} --set Token=${CODEFRESH_TOKEN}"
-codefresh runner $ACTION -y --values ${VALUES_FILE} $ACTION_PARAMETERS --set Token=${CODEFRESH_TOKEN}
+COMMAND="codefresh runner $ACTION -y --values ${VALUES_FILE} $ACTION_PARAMETERS"
+echo "Executing $COMMAND ..."
 
-# sleep 1800
+eval $COMMAND $CODEFRESH_TOKEN_PARAM
+
+#sleep 1800
 
